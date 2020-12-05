a27c9a766d8b97c1c0a0f75b47a488382a82cfd3
Eugene Korbut <me@mvl.ru>
2020-12-05 11:35:27 +0300

    Binary parser

--- src/day05/main.rs
+++ src/day05/main.rs
@@ -1,3 +1,31 @@
+fn binary(input: &str, one: char, zero: char) -> Option<u32> {
+    let size = input.len();
+    let mut result: u32 = 0;
+
+    for (i, c) in input.chars().enumerate() {
+        let shift = size - 1 - i;
+        match c {
+            c if c == one => result |= 1 << shift,
+            c if c == zero => {}
+            _ => return None,
+        }
+    }
+
+    Some(result)
+}
+
 fn main() {
     println!("test me");
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_binary() {
+        assert_eq!(binary("10", '1', '0'), Some(2));
+        assert_eq!(binary("1*0", '1', '0'), None);
+        assert_eq!(binary("FBFBBFF", 'B', 'F'), Some(44));
+    }
 }
