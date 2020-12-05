ee2cd226eb662a5b4ed85cf47db0bf1b965ee0ac
Eugene Korbut <me@mvl.ru>
2020-12-05 11:42:57 +0300

    BoardPass parsing

--- src/day05/main.rs
+++ src/day05/main.rs
@@ -14,6 +14,25 @@ fn binary(input: &str, one: char, zero: char) -> Option<u32> {
     Some(result)
 }
 
+#[derive(PartialEq, Debug)]
+struct BoardPass {
+    row: u32,
+    column: u32,
+}
+
+impl BoardPass {
+    fn new(row: u32, column: u32) -> Self {
+        Self { row, column }
+    }
+
+    pub fn parse(input: &str) -> Option<Self> {
+        let row = binary(&input[0..7], 'B', 'F')?;
+        let column = binary(&input[7..10], 'R', 'L')?;
+
+        Some(Self::new(row, column))
+    }
+}
+
 fn main() {
     println!("test me");
 }
@@ -27,5 +46,13 @@ mod test {
         assert_eq!(binary("10", '1', '0'), Some(2));
         assert_eq!(binary("1*0", '1', '0'), None);
         assert_eq!(binary("FBFBBFF", 'B', 'F'), Some(44));
+    }
+
+    #[test]
+    fn test_board_pass() {
+        assert_eq!(
+            BoardPass::parse("BFFFBBFRRR").unwrap(),
+            BoardPass::new(70, 7)
+        );
     }
 }
