f7cadadd6c8e7440a3e012b83977d073bc736dbb
Eugene Korbut <me@mvl.ru>
2020-12-06 11:14:39 +0300

    6/2 not working

--- src/day06/main.rs
+++ src/day06/main.rs
@@ -3,37 +3,67 @@ use std::fs;
 
 struct Group {
     group_answers: HashSet<char>,
+    people: Vec<HashSet<char>>,
 }
 
 impl Group {
     pub fn parse(input: &str) -> Self {
         let mut group_answers = HashSet::new();
+        let mut people = vec![];
 
-        for c in input.chars() {
-            match c {
-                'a'..='z' => {
-                    group_answers.insert(c);
+        for p in input.lines() {
+            let mut person = HashSet::new();
+
+            for c in p.chars() {
+                match c {
+                    'a'..='z' => {
+                        group_answers.insert(c);
+                        person.insert(c);
+                    }
+                    _ => {}
                 }
-                _ => {}
+            }
+
+            if person.len() > 0 {
+                people.push(person);
             }
         }
 
-        Self { group_answers }
+        Self {
+            group_answers,
+            people,
+        }
     }
 
-    pub fn any_ansered(&self) -> usize {
+    pub fn any_answered(&self) -> usize {
         self.group_answers.len()
+    }
+
+    pub fn all_answered(&self) -> usize {
+        let total = self.any_answered();
+
+        self.group_answers
+            .iter()
+            .filter(|answer| self.answered_to(answer) == total)
+            .count()
+    }
+
+    fn answered_to(&self, answer: &char) -> usize {
+        self.people
+            .iter()
+            .filter(|person| person.contains(answer))
+            .count()
     }
 }
 
 fn main() {
     let data = fs::read_to_string("data/day06.txt").expect("File not found");
 
-    let foo: usize = data
-        .split("\n\n")
-        .map(Group::parse)
-        .map(|group| group.any_ansered())
-        .sum();
+    let groups = data.split("\n\n").map(Group::parse).collect::<Vec<_>>();
 
-    dbg!(foo);
+    let task_a: usize = groups.iter().map(|g| g.any_answered()).sum();
+    let task_b: usize = groups.iter().map(|g| g.all_answered()).sum();
+
+    dbg!(task_a);
+    dbg!(task_b);
 }
