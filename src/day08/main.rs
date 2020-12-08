5649d66fc7f3a364aba1ab96b0a162b27d6d8580
Eugene Korbut <me@mvl.ru>
2020-12-08 11:25:12 +0300

    08 / 01

--- src/day08/main.rs
+++ src/day08/main.rs
@@ -1,6 +1,8 @@
 use lazy_static::lazy_static;
 use regex::Regex;
+use std::collections::HashSet;
 use std::convert::TryFrom;
+
 type Error = Box<dyn std::error::Error>;
 
 #[derive(Debug, PartialEq)]
@@ -20,20 +22,13 @@ impl TryFrom<&str> for Insruction {
 
         let err = || format!("invald input: {}", input);
 
-        let captures = RE.captures(input).ok_or_else(|| "Invalid input")?;
-        dbg!(&captures);
-
+        let captures = RE.captures(input).ok_or_else(err)?;
         let op = captures.get(1).map(|c| c.as_str()).ok_or_else(err)?;
-
-        dbg!(op);
-
         let value = captures
             .get(2)
             .map(|c| c.as_str())
             .and_then(|c| c.parse::<i32>().ok())
             .ok_or_else(err)?;
-
-        dbg!(value);
 
         match (op, value) {
             ("nop", _) => Ok(Insruction::Noop),
@@ -44,8 +39,86 @@ impl TryFrom<&str> for Insruction {
     }
 }
 
+type Programm = Vec<Insruction>;
+
+fn parse(input: &str) -> Result<Programm, Error> {
+    input.lines().map(Insruction::try_from).collect()
+}
+
+struct CPU {
+    prog: Programm,
+    ip: usize,
+    acc: i32,
+}
+
+impl CPU {
+    pub fn new(prog: Programm) -> Self {
+        Self {
+            prog,
+            ip: 0,
+            acc: 0,
+        }
+    }
+
+    pub fn ip(&self) -> usize {
+        self.ip
+    }
+
+    pub fn acc(&self) -> i32 {
+        self.acc
+    }
+
+    pub fn step(&mut self) {
+        dbg!(&self.ip);
+        dbg!(&self.prog[self.ip]);
+        match self.prog[self.ip] {
+            Insruction::Noop => {
+                self.ip += 1;
+            }
+            Insruction::Acc(v) => {
+                self.ip += 1;
+                self.acc += v;
+            }
+            Insruction::Jmp(v) => {
+                let next = self.ip as i32 + v;
+                self.ip = next as usize;
+            }
+        }
+    }
+}
+
+struct Solver {
+    cpu: CPU,
+}
+
+impl Solver {
+    pub fn new(prog: Programm) -> Self {
+        Self {
+            cpu: CPU::new(prog),
+        }
+    }
+
+    pub fn solve(&mut self) -> i32 {
+        let mut visited: HashSet<usize> = HashSet::new();
+
+        loop {
+            self.cpu.step();
+            let ip = self.cpu.ip();
+
+            if visited.contains(&ip) {
+                return self.cpu.acc();
+            }
+
+            visited.insert(ip);
+        }
+    }
+}
+
 fn main() {
-    println!("Hello");
+    let data = std::fs::read_to_string("data/day08.txt").unwrap();
+    let programm = parse(&data).unwrap();
+    let value = Solver::new(programm).solve();
+    dbg!(value);
 }
 
 #[cfg(test)]
