6d96779c7167936dc8f97fc9e04916192cbb31b1
Eugene Korbut <me@mvl.ru>
2020-12-10 11:34:12 +0300

    day 10 / task 2

--- src/day10/main.rs
+++ src/day10/main.rs
@@ -35,11 +35,63 @@ fn solve_a(input: &[isize]) -> Result<isize, Box<dyn Error>> {
     Ok(result)
 }
 
+fn solve_b(input: &[isize]) -> Result<isize, Box<dyn Error>> {
+    let max = *input.last().ok_or_else(|| "Last not found")?;
+    let last = std::iter::once(max + 3);
+    let first = std::iter::once(0);
+
+    let input = first
+        .chain(input.iter().map(|v| *v))
+        .chain(last)
+        .collect::<Vec<_>>();
+
+    let mut cache = HashMap::new();
+    let v = permutate_rec(input.as_slice(), &mut cache, 0);
+
+    Ok(v)
+}
+
+fn permutate_rec(
+    input: &[isize],
+    cache: &mut HashMap<usize, isize>,
+    index_from_start: usize,
+) -> isize {
+    if let Some(i) = cache.get(&index_from_start) {
+        return *i;
+    }
+
+    if input.len() == 1 {
+        cache.insert(index_from_start, 1);
+        return 1;
+    }
+
+    let start = input[0];
+
+    let result = input
+        .iter()
+        .enumerate()
+        .skip(1)
+        .take_while(|(_, v)| **v - start <= 3)
+        .map(|(index, v)| {
+            // println!("{} -> {} (index {})", start, v, index);
+
+            permutate_rec(&input[index..], cache, index_from_start + index)
+        })
+        .sum();
+
+    cache.insert(index_from_start, result);
+
+    return result;
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let input = read_input()?;
-    let task_a = solve_a(&input)?;
 
+    let task_a = solve_a(&input)?;
     dbg!(task_a);
+
+    let task_b = solve_b(&input)?;
+    dbg!(task_b);
 
     Ok(())
 }
