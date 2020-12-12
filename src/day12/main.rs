e11a3eb2dc277596445be20ac14ff1e607505b0d
Eugene Korbut <me@mvl.ru>
2020-12-12 20:31:38 +0300

    Task A

--- src/day12/main.rs
+++ src/day12/main.rs
@@ -1,6 +1,8 @@
 use std::convert::TryFrom;
 use std::error::Error;
+use std::fs;
 
+#[derive(Debug, Clone, Copy)]
 enum Direction {
     North,
     East,
@@ -8,6 +10,7 @@ enum Direction {
     West,
 }
 
+#[derive(Debug)]
 enum Action {
     North(isize),
     South(isize),
@@ -33,11 +36,84 @@ impl TryFrom<&str> for Action {
             'L' => Ok(Action::Left(value)),
             'R' => Ok(Action::Right(value)),
             'F' => Ok(Action::Forward(value)),
-            _ => Err(format!("Invalid input: {}", input)),
+            _ => Err(format!("Invalid input: {}", input).into()),
         }
     }
 }
 
-fn main() {
-    println!("Day 12 placeholder")
+#[derive(Debug)]
+struct World {
+    x: isize,
+    y: isize,
+    dir: Direction,
+}
+
+impl World {
+    fn new() -> Self {
+        Self {
+            x: 0,
+            y: 0,
+            dir: Direction::East,
+        }
+    }
+
+    fn step(&mut self, action: &Action) {
+        // let foo: [i8; 4] = [0, 1, 2, 3];
+        // let bar = foo[8];
+
+        match action {
+            Action::North(value) => self.y -= value,
+            Action::South(value) => self.y += value,
+            Action::East(value) => self.x += value,
+            Action::West(value) => self.x -= value,
+            Action::Forward(value) => self.step_forward(*value),
+            Action::Left(value) => self.turn(*value * -1),
+            Action::Right(value) => self.turn(*value),
+        }
+    }
+
+    fn step_forward(&mut self, value: isize) {
+        let next_action = match self.dir {
+            Direction::North => Action::North(value),
+            Direction::East => Action::East(value),
+            Direction::South => Action::South(value),
+            Direction::West => Action::West(value),
+        };
+
+        self.step(&next_action);
+    }
+
+    fn turn(&mut self, angle: isize) {
+        let before = match self.dir {
+            Direction::North => 0,
+            Direction::East => 1,
+            Direction::South => 2,
+            Direction::West => 3,
+        };
+
+        let angle = (angle / 90) % 4;
+        let next_angel = ((((before + angle) % 4) + 4) % 4) as usize;
+        self.dir = [
+            Direction::North,
+            Direction::East,
+            Direction::South,
+            Direction::West,
+        ][next_angel];
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let data = fs::read_to_string("data/day12.txt")?;
+    let input = data
+        .lines()
+        .map(Action::try_from)
+        .collect::<Result<Vec<_>, _>>()?;
+
+    let mut world = World::new();
+
+    for action in input.iter() {
+        world.step(action);
+    }
+
+    Ok(())
 }
