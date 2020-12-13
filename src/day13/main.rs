897042537bb8bd10cd612f46c75337714c34cb15
Eugene Korbut <me@mvl.ru>
2020-12-13 21:46:19 +0300

    Task B, varian 1

--- src/day13/main.rs
+++ src/day13/main.rs
@@ -23,7 +23,8 @@ impl TryFrom<&str> for Bus {
     }
 }
 
-struct Input {
+#[derive(Debug)]
+pub struct Input {
     start_time: usize,
     buses: Vec<Bus>,
 }
@@ -43,18 +44,84 @@ impl TryFrom<&str> for Input {
                 .map(Bus::try_from)
                 .collect::<Result<Vec<_>, _>>()
         })?;
-        dbg!(buses);
-        dbg!(start_time);
 
-        Ok(Self {
-            start_time,
-            buses: vec![],
-        })
+        Ok(Self { start_time, buses })
+    }
+}
+
+mod solver {
+    pub fn solve_a(input: &crate::Input) -> Option<usize> {
+        (input.start_time..)
+            .find_map(|time| {
+                input.buses.iter().find_map(|item| match item {
+                    crate::Bus::Active(n) if time % n == 0 => Some(((time - input.start_time), n)),
+                    _ => None,
+                })
+            })
+            .map(|(a, b)| {
+                dbg!(a);
+                dbg!(b);
+                a * b
+            })
+    }
+
+    pub fn solve_b(input: &crate::Input) -> Option<i128> {
+        let mut target = input
+            .buses
+            .iter()
+            .enumerate()
+            .filter_map(|(index, v)| {
+                if let crate::Bus::Active(n) = v {
+                    Some((index, *n))
+                } else {
+                    None
+                }
+            })
+            .collect::<Vec<_>>();
+
+        target.sort_by_key(|(_, bus_n)| *bus_n);
+        target.reverse();
+
+        dbg!(&target);
+
+        // let mut i: i128 = 100000000000000;
+        let mut i: i128 = 0;
+        let step = target.get(0).map(|(_, bus_n)| *bus_n)?;
+
+        loop {
+            let founded = target
+                .iter()
+                .filter_map(|(index, bus_n)| {
+                    let rem = (i + *index as i128) % (*bus_n as i128);
+                    if rem == 0 {
+                        None
+                    } else {
+                        Some(*bus_n as i128 - rem)
+                    }
+                })
+                .max();
+
+            if let Some(n) = founded {
+                println!("{} -> {} ({})", i, i + n, n);
+                i += n;
+            } else {
+                return Some(i);
+            }
+        }
+
+        // (0..).find(|n| target.iter().all(|(index, bus_n)| (n + index) % bus_n == 0))
     }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let raw = fs::read_to_string("data/day13.txt")?;
     let input: Input = raw[..].try_into()?;
+
+    let task_a = solver::solve_a(&input).ok_or("Task A not solved")?;
+    dbg!(task_a);
+
+    let task_b = solver::solve_b(&input).ok_or("Task B not solved")?;
+    dbg!(task_b);
+
     Ok(())
 }
