7f745720b2eb1c96c2950af167b68b6e06045a1a
Eugene Korbut <me@mvl.ru>
2020-12-19 21:27:56 +0300

    some code

--- src/day11/main.rs
+++ src/day11/main.rs
@@ -4,19 +4,51 @@ use std::fs;
 #[derive(Debug)]
 struct Grid<T>
 where
-    T: std::fmt::Debug,
+    T: std::fmt::Debug + std::cmp::PartialEq,
 {
     data: Vec<T>,
     width: usize,
     height: usize,
 }
 
-// impl Grid {}
+impl<T> Grid<T>
+where
+    T: std::fmt::Debug + std::cmp::PartialEq,
+{
+    fn step<F>(&mut self, changer: F) -> usize
+    where
+        F: Fn(&Grid<T>, usize, usize) -> T,
+    {
+        let mut next = vec![];
+        let mut changed = 0;
+        for y in 0..self.height {
+            for x in 0..self.width {
+                let current = self.get(x, y);
+                let next_item = changer(&self, x, y);
+                if *current != next_item {
+                    changed += 1
+                }
+                next.push(next_item);
+            }
+        }
+
+        self.data = next;
+        changed
+    }
+
+    fn get(&self, x: usize, y: usize) -> &T {
+        assert!(x <= self.width);
+        assert!(y <= self.height);
+
+        let index = x + y * self.width;
+        &self.data[index]
+    }
+}
 
 fn parse_grid<T, F>(input: &str, parser: F) -> Option<Grid<T>>
 where
     F: Fn(char) -> Option<T>,
-    T: std::fmt::Debug,
+    T: std::fmt::Debug + std::cmp::PartialEq,
 {
     let mut width: Option<usize> = None;
     let mut height = 0;
@@ -43,7 +75,7 @@ where
     })
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum Seat {
     Floor,
     Empty,
@@ -59,9 +91,25 @@ fn parser(input: char) -> Option<Seat> {
     }
 }
 
+fn step(grid: &Grid<Seat>, x: usize, y: usize) -> Seat {
+    Seat::Empty
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let data = fs::read_to_string("data/day11.txt")?;
-    let grid = parse_grid(&data, parser);
+    let mut grid = parse_grid(&data, parser).ok_or("parsing error")?;
+    let mut steps = 0;
+
+    loop {
+        let changed = grid.step(step);
+        if changed == 0 {
+            break;
+        }
+
+        steps += 1;
+    }
+
+    dbg!(steps);
 
     Ok(())
 }
