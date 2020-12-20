7a4fbf446784af227f530051da4f190f217cd476
Eugene Korbut <me@mvl.ru>
2020-12-20 12:15:45 +0300

    Day 11 done

--- src/day11/main.rs
+++ src/day11/main.rs
@@ -31,7 +31,7 @@ fn parser(input: char) -> Option<Seat> {
     }
 }
 
-fn step(grid: &Grid<Seat>, x: usize, y: usize) -> Seat {
+fn step_task_a(grid: &Grid<Seat>, x: usize, y: usize) -> Seat {
     let occupied = Direction::all()
         .filter_map(|d| d.iter(x, y).next())
         .filter_map(|(x, y)| grid.get(x, y))
@@ -57,12 +57,43 @@ fn step(grid: &Grid<Seat>, x: usize, y: usize) -> Seat {
     }
 }
 
+fn step_task_b(grid: &Grid<Seat>, x: usize, y: usize) -> Seat {
+    let occupied = Direction::all()
+        .filter_map(|d| {
+            d.iter(x, y)
+                .scan((), |(), (x, y)| grid.get(x, y))
+                .fuse()
+                .skip_while(|s| **s == Seat::Floor)
+                .next()
+        })
+        .filter(|s| **s == Seat::Occupied)
+        .count();
+
+    match grid.get(x as usize, y as usize).unwrap() {
+        Seat::Empty => {
+            if occupied == 0 {
+                Seat::Occupied
+            } else {
+                Seat::Empty
+            }
+        }
+        Seat::Occupied => {
+            if occupied >= 5 {
+                Seat::Empty
+            } else {
+                Seat::Occupied
+            }
+        }
+        Seat::Floor => Seat::Floor,
+    }
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let data = fs::read_to_string("data/day11.txt")?;
     let mut grid = Grid::parse(&data, parser).ok_or("parsing error")?;
 
     loop {
-        let changed = grid.step(step);
+        let changed = grid.step(step_task_a);
 
         if changed == 0 {
             break;
@@ -71,6 +102,19 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let task_a = grid.iter().filter(|item| **item == Seat::Occupied).count();
     dbg!(task_a);
+
+    let mut grid = Grid::parse(&data, parser).ok_or("parsing error")?;
+
+    loop {
+        let changed = grid.step(step_task_b);
+
+        if changed == 0 {
+            break;
+        }
+    }
+
+    let task_b = grid.iter().filter(|item| **item == Seat::Occupied).count();
+    dbg!(task_b);
 
     Ok(())
 }
