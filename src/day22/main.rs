7d301de1515f5dddcf6df983d7211bacd8d09159
Eugene Korbut <me@mvl.ru>
2020-12-22 17:03:00 +0300

    Day 22 - recursive play

--- src/day22/main.rs
+++ src/day22/main.rs
@@ -1,9 +1,10 @@
 use std::collections::VecDeque;
 use std::convert::TryFrom;
 use std::error::Error;
+use std::fmt;
 use std::fs;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Deck(VecDeque<usize>);
 
 fn parse_deck(input: &str) -> Result<Deck, Box<dyn Error>> {
@@ -19,6 +20,19 @@ fn parse_deck(input: &str) -> Result<Deck, Box<dyn Error>> {
     Ok(Deck(cards))
 }
 
+impl fmt::Display for Deck {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let cards = self
+            .0
+            .iter()
+            .map(|v| format!("{}", v))
+            .collect::<Vec<_>>()
+            .join(", ");
+
+        write!(f, "[{}]", cards)
+    }
+}
+
 impl Deck {
     pub fn score(&self) -> usize {
         self.0
@@ -30,18 +44,33 @@ impl Deck {
     }
 
     pub fn top(&mut self) -> Option<usize> {
-        self.0.get(0).copied()
+        self.0.pop_front()
     }
 
-    pub fn change(&mut self, first: usize, second: usize) {
-        let _ = self.0.pop_front();
+    pub fn add(&mut self, first: usize, second: usize) {
         self.0.push_back(first);
         self.0.push_back(second);
     }
 
-    pub fn remove_top(&mut self) {
-        let _ = self.0.pop_front();
+    pub fn add_front(&mut self, value: usize) {
+        self.0.push_back(value)
     }
+
+    pub fn has(&self, value: usize) -> bool {
+        self.0.len() >= value
+    }
+
+    pub fn copy(&self, value: usize) -> Deck {
+        assert!(self.0.len() >= value);
+        let copy = self.0.iter().take(value).copied().collect::<VecDeque<_>>();
+
+        Deck(copy)
+    }
+}
+
+enum GameResult {
+    WinA,
+    WinB,
 }
 
 enum TurnResult {
@@ -50,10 +79,16 @@ enum TurnResult {
     Continue,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Game {
     player_a: Deck,
     player_b: Deck,
+}
+
+impl fmt::Display for Game {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "A: {}, B: {}", self.player_a, self.player_b)
+    }
 }
 
 impl TryFrom<&str> for Game {
@@ -69,6 +104,10 @@ impl TryFrom<&str> for Game {
 }
 
 impl Game {
+    pub fn new(player_a: Deck, player_b: Deck) -> Self {
+        Self { player_a, player_b }
+    }
+
     pub fn play(&mut self) -> usize {
         loop {
             match self.turn() {
@@ -76,6 +115,15 @@ impl Game {
                 TurnResult::WinB => return self.player_b.score(),
                 _ => {}
             }
+        }
+    }
+
+    pub fn play_recursive(&mut self) -> usize {
+        println!("Play recursive: {}", self);
+
+        match self.game_recursive() {
+            GameResult::WinA => self.player_a.score(),
+            GameResult::WinB => self.player_b.score(),
         }
     }
 
@@ -87,15 +135,64 @@ impl Game {
 
         let b = match self.player_b.top() {
             Some(b) => b,
-            _ => return TurnResult::WinA,
+            _ => {
+                self.player_a.add_front(a);
+                return TurnResult::WinA;
+            }
         };
 
         if a > b {
-            self.player_a.change(a, b);
-            self.player_b.remove_top();
+            self.player_a.add(a, b);
         } else if a < b {
-            self.player_b.change(b, a);
-            self.player_a.remove_top();
+            self.player_b.add(b, a);
+        } else {
+            panic!("Equal cards")
+        }
+
+        TurnResult::Continue
+    }
+
+    fn game_recursive(&mut self) -> GameResult {
+        println!("Game recursive: {}", self);
+        loop {
+            match self.turn_recursive() {
+                TurnResult::WinA => return GameResult::WinA,
+                TurnResult::WinB => return GameResult::WinB,
+                _ => {}
+            }
+        }
+    }
+
+    fn turn_recursive(&mut self) -> TurnResult {
+        println!("Turn: {}", self);
+
+        let a = match self.player_a.top() {
+            Some(a) => a,
+            _ => return TurnResult::WinB,
+        };
+
+        let b = match self.player_b.top() {
+            Some(b) => b,
+            _ => {
+                self.player_a.add_front(a);
+                return TurnResult::WinA;
+            }
+        };
+
+        if self.player_a.has(a) && self.player_b.has(b) {
+            let next_a = self.player_a.copy(a);
+            let next_b = self.player_b.copy(b);
+
+            let mut game = Game::new(next_a, next_b);
+
+            match game.game_recursive() {
+                GameResult::WinA => self.player_a.add(a, b),
+                GameResult::WinB => self.player_b.add(b, a),
+            }
+        } else if a > b {
+            self.player_a.add(a, b);
+        } else if a < b {
+            self.player_b.add(b, a);
         } else {
             panic!("Equal cards")
         }
@@ -106,9 +203,13 @@ impl Game {
 
 fn main() -> Result<(), Box<dyn Error>> {
     let input = fs::read_to_string("data/day22.txt")?;
-    let mut game = Game::try_from(input.as_ref())?;
+    let mut game_a = Game::try_from(input.as_ref())?;
+    let mut game_b = game_a.clone();
 
-    let task_a = game.play();
+    let task_a = game_a.play();
+    let task_b = game_b.play_recursive();
+
     println!("Task A: {}", task_a);
+    println!("Task B: {}", task_b);
     Ok(())
 }
