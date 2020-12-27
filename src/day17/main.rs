18fa8563d045db821cd212f06c5961d97bafd9c4
Eugene Korbut <me@mvl.ru>
2020-12-27 20:44:47 +0300

    Some code

--- src/day17/main.rs
+++ src/day17/main.rs
@@ -1,3 +1,15 @@
+struct Point {
+    x: isize,
+    y: isize,
+    z: isize,
+}
+
+impl Point {
+    pub fn new(x: isize, y: isize, z: isize) -> Self {
+        Self { x, y, z }
+    }
+}
+
 fn main() {
     println!("Just placeholder");
 }
