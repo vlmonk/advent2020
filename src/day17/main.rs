3f60351da90b1c43cd32922fe589f3428018c732
Eugene Korbut <me@mvl.ru>
2020-12-27 23:20:29 +0300

    Day 17 / part 1

--- src/day17/main.rs
+++ src/day17/main.rs
@@ -1,17 +1,8 @@
 use advent2020::grid::Grid;
 use std::collections::HashSet;
 use std::error::Error;
+use std::fmt;
 use std::fs;
-
-fn extend_min(a: Option<isize>, b: isize) -> Option<isize> {
-    let value = if let Some(a) = a { a } else { b };
-    Some(value.min(b - 1))
-}
-
-fn extend_max(a: Option<isize>, b: isize) -> Option<isize> {
-    let value = if let Some(a) = a { a } else { b };
-    Some(value.max(b + 1))
-}
 
 #[derive(PartialEq, Eq, Hash, Debug)]
 struct Point {
@@ -28,6 +19,9 @@ impl Point {
     }
 
     pub fn around(&self) -> impl Iterator<Item = Point> {
+        let px = self.x;
+        let py = self.y;
+        let pz = self.z;
         AROUND
             .iter()
             .map(|x| AROUND.iter().map(move |y| (*x, *y)))
@@ -35,31 +29,84 @@ impl Point {
             .map(|(x, y)| AROUND.iter().map(move |z| (x, y, *z)))
             .flatten()
             .filter(|(x, y, z)| *x != 0 || *y != 0 || *z != 0)
-            .map(|(x, y, z)| Point::new(x, y, z))
+            .map(move |(x, y, z)| Point::new(x + px, y + py, z + pz))
+    }
+}
+
+#[derive(Debug)]
+struct Dimension {
+    min_x: isize,
+    max_x: isize,
+    min_y: isize,
+    max_y: isize,
+    min_z: isize,
+    max_z: isize,
+}
+
+impl Dimension {
+    pub fn extend(&mut self, point: &Point) {
+        self.min_x = self.min_x.min(point.x - 1);
+        self.max_x = self.max_x.max(point.x + 1);
+        self.min_y = self.min_y.min(point.y - 1);
+        self.max_y = self.max_y.max(point.y + 1);
+        self.min_z = self.min_z.min(point.z - 1);
+        self.max_z = self.max_z.max(point.z + 1);
+    }
+
+    pub fn from_point(point: &Point) -> Self {
+        Self {
+            min_x: point.x - 1,
+            max_x: point.x + 1,
+            min_y: point.y - 1,
+            max_y: point.y + 1,
+            min_z: point.z - 1,
+            max_z: point.z + 1,
+        }
     }
 }
 
 #[derive(Debug)]
 struct Space {
     world: HashSet<Point>,
-    min_x: Option<isize>,
-    max_x: Option<isize>,
-    min_y: Option<isize>,
-    max_y: Option<isize>,
-    min_z: Option<isize>,
-    max_z: Option<isize>,
+    dimension: Option<Dimension>,
+}
+
+impl fmt::Display for Space {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        if let Some(d) = self.dimension.as_ref() {
+            writeln!(
+                f,
+                "Dimension: X: {}:{}, Y: {}:{}, Z: {}:{}",
+                d.min_x, d.max_x, d.min_y, d.max_y, d.min_z, d.max_z
+            );
+
+            for z in d.min_z + 1..d.max_z {
+                write!(f, "Layer {}\n", z);
+                for y in d.min_y + 1..d.max_y {
+                    for x in d.min_x + 1..d.max_x {
+                        if self.active(&Point::new(x, y, z)) {
+                            write!(f, "#");
+                        } else {
+                            write!(f, ".");
+                        }
+                    }
+
+                    write!(f, "\n");
+                }
+            }
+
+            Ok(())
+        } else {
+            write!(f, "Empty fields")
+        }
+    }
 }
 
 impl Space {
-    pub fn new() -> Self {
+    fn new() -> Self {
         Self {
             world: HashSet::new(),
-            min_x: None,
-            max_x: None,
-            min_y: None,
-            max_y: None,
-            min_z: None,
-            max_z: None,
+            dimension: None,
         }
     }
 
@@ -70,22 +117,73 @@ impl Space {
                 if let Some(Element::Active) = input.get(x, y) {
                     let point = Point::new(x as isize, y as isize, 0);
                     space.add_point(point);
+                    // dbg!(&space.dimension);
                 }
             }
         }
         space
     }
 
-    pub fn add_point(&mut self, point: Point) {
-        self.min_x = extend_min(self.min_x, point.x);
-        self.min_y = extend_min(self.min_y, point.y);
-        self.min_z = extend_min(self.min_z, point.z);
-
-        self.max_x = extend_max(self.max_x, point.x);
-        self.max_y = extend_max(self.max_y, point.y);
-        self.max_z = extend_max(self.min_z, point.z);
+    fn add_point(&mut self, point: Point) {
+        match self.dimension.as_mut() {
+            Some(d) => d.extend(&point),
+            None => self.dimension = Some(Dimension::from_point(&point)),
+        }
 
         self.world.insert(point);
+    }
+
+    fn active(&self, p: &Point) -> bool {
+        self.world.contains(p)
+    }
+
+    fn total(&self) -> usize {
+        self.world.len()
+    }
+
+    pub fn step(&self) -> Self {
+        let mut space = Space::new();
+
+        for point in self.points() {
+            let around = point.around().filter(|p| self.active(p)).count();
+
+            if self.active(&point) {
+                if around == 2 || around == 3 {
+                    space.add_point(point)
+                }
+            } else {
+                if around == 3 {
+                    space.add_point(point)
+                }
+            }
+        }
+
+        // dbg!(&self);
+        space
+    }
+
+    fn points(&self) -> Box<dyn Iterator<Item = Point>> {
+        if let Some(d) = &self.dimension {
+            let min_x = d.min_x;
+            let max_x = d.max_x;
+
+            let min_y = d.min_y;
+            let max_y = d.max_y;
+
+            let min_z = d.min_z;
+            let max_z = d.max_z;
+
+            let iter = (min_x..=max_x)
+                .into_iter()
+                .map(move |x| (min_y..=max_y).into_iter().map(move |y| (x, y)))
+                .flatten()
+                .map(move |(x, y)| (min_z..=max_z).into_iter().map(move |z| (x, y, z)))
+                .flatten()
+                .map(|(x, y, z)| Point::new(x, y, z));
+            Box::new(iter)
+        } else {
+            Box::new(std::iter::empty::<Point>())
+        }
     }
 }
 
@@ -106,9 +204,16 @@ fn parser(input: char) -> Option<Element> {
 fn main() -> Result<(), Box<dyn Error>> {
     let raw = fs::read_to_string("data/day17.txt")?;
     let grid = Grid::parse(&raw, parser).ok_or("can't parse input")?;
-    let space = Space::from_grid(grid);
+    let mut space = Space::from_grid(grid);
 
-    dbg!(space);
+    println!("before:\n{}", space);
+
+    for step in 0..6 {
+        space = space.step();
+        // dbg!(&space);
+    }
+
+    dbg!(space.total());
 
     Ok(())
 }
