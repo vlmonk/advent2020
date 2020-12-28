3590cf10351bad566162b8e87de69bb5ebba0095
Eugene Korbut <me@mvl.ru>
2020-12-29 00:36:45 +0300

    Massive refactor

--- src/day17/main.rs
+++ src/day17/main.rs
@@ -3,8 +3,23 @@ use std::collections::HashSet;
 use std::error::Error;
 use std::fmt;
 use std::fs;
+use std::ops::Sub;
 
-#[derive(PartialEq, Eq, Hash, Debug)]
+trait Point: Clone + Eq + core::hash::Hash {
+    fn from_xy(x: isize, y: isize) -> Self;
+    fn around(&self) -> Box<dyn Iterator<Item = Self>>;
+    fn extend_min(&mut self, p: &Self);
+    fn extend_max(&mut self, p: &Self);
+}
+
+trait DimensionBound {
+    fn add(&self, rhs: &Self) -> Self;
+    fn sub(&self, rhs: &Self) -> Self;
+    fn one() -> Self;
+    fn all(&self, hrs: &Self) -> Box<dyn Iterator<Item = Self>>;
+}
+
+#[derive(PartialEq, Eq, Hash, Debug, Clone)]
 struct Point3 {
     x: isize,
     y: isize,
@@ -17,92 +32,155 @@ impl Point3 {
     pub fn new(x: isize, y: isize, z: isize) -> Self {
         Point3 { x, y, z }
     }
+}
 
-    pub fn around(&self) -> impl Iterator<Item = Point3> {
+impl DimensionBound for Point3 {
+    fn add(&self, hrs: &Self) -> Self {
+        Self::new(self.x + hrs.x, self.y + hrs.y, self.z + hrs.z)
+    }
+
+    fn sub(&self, hrs: &Self) -> Self {
+        Self::new(self.x - hrs.x, self.y - hrs.y, self.z - hrs.z)
+    }
+
+    fn one() -> Self {
+        Self::new(1, 1, 1)
+    }
+
+    fn all(&self, hrs: &Self) -> Box<dyn Iterator<Item = Self>> {
+        let min_x = self.x;
+        let max_x = hrs.x;
+
+        let min_y = self.y;
+        let max_y = hrs.y;
+
+        let min_z = self.z;
+        let max_z = hrs.z;
+
+        let iter = (min_x..=max_x)
+            .into_iter()
+            .map(move |x| (min_y..=max_y).into_iter().map(move |y| (x, y)))
+            .flatten()
+            .map(move |(x, y)| (min_z..=max_z).into_iter().map(move |z| (x, y, z)))
+            .flatten()
+            .map(|(x, y, z)| {
+                // println!("{}/{}/{}", x, y, z);
+                Point3::new(x, y, z)
+            });
+        Box::new(iter)
+    }
+}
+
+impl Point for Point3 {
+    fn from_xy(x: isize, y: isize) -> Self {
+        Self { x, y, z: 0 }
+    }
+
+    fn around(&self) -> Box<dyn Iterator<Item = Self>> {
         let px = self.x;
         let py = self.y;
         let pz = self.z;
-        AROUND
+
+        let iter = AROUND
             .iter()
             .map(|x| AROUND.iter().map(move |y| (*x, *y)))
             .flatten()
             .map(|(x, y)| AROUND.iter().map(move |z| (x, y, *z)))
             .flatten()
             .filter(|(x, y, z)| *x != 0 || *y != 0 || *z != 0)
-            .map(move |(x, y, z)| Point3::new(x + px, y + py, z + pz))
+            .map(move |(x, y, z)| Point3::new(x + px, y + py, z + pz));
+
+        Box::new(iter)
+    }
+
+    fn extend_min(&mut self, p: &Self) {
+        self.x = self.x.min(p.x);
+        self.y = self.y.min(p.y);
+        self.z = self.z.min(p.z);
+    }
+
+    fn extend_max(&mut self, p: &Self) {
+        self.x = self.x.max(p.x);
+        self.y = self.y.max(p.y);
+        self.z = self.z.max(p.z);
+    }
+}
+
+impl Sub<isize> for Point3 {
+    type Output = Self;
+    fn sub(self, other: isize) -> Self {
+        Point3::new(self.x - other, self.y - other, self.z - other)
     }
 }
 
 #[derive(Debug)]
-struct Dimension {
-    min_x: isize,
-    max_x: isize,
-    min_y: isize,
-    max_y: isize,
-    min_z: isize,
-    max_z: isize,
+struct Dimension<T>
+where
+    T: Point + DimensionBound,
+{
+    min: T,
+    max: T,
 }
 
-impl Dimension {
-    pub fn extend(&mut self, point: &Point3) {
-        self.min_x = self.min_x.min(point.x - 1);
-        self.max_x = self.max_x.max(point.x + 1);
-        self.min_y = self.min_y.min(point.y - 1);
-        self.max_y = self.max_y.max(point.y + 1);
-        self.min_z = self.min_z.min(point.z - 1);
-        self.max_z = self.max_z.max(point.z + 1);
+impl<T> Dimension<T>
+where
+    T: Point + DimensionBound + Clone,
+{
+    pub fn extend(&mut self, point: &T) {
+        self.min.extend_min(point);
+        self.max.extend_max(point);
     }
 
-    pub fn from_point(point: &Point3) -> Self {
+    pub fn from_point(point: &T) -> Self {
         Self {
-            min_x: point.x - 1,
-            max_x: point.x + 1,
-            min_y: point.y - 1,
-            max_y: point.y + 1,
-            min_z: point.z - 1,
-            max_z: point.z + 1,
+            min: point.clone(),
+            max: point.clone(),
         }
     }
+
+    pub fn points(&self) -> Box<dyn Iterator<Item = T>> {
+        let min = self.min.sub(&T::one());
+        let max = self.max.add(&T::one());
+
+        min.all(&max)
+    }
 }
+
+// impl Dimension<Point3> {
+//     pub fn points(&self) -> Box<dyn Iterator<Item = Point3>> {
+//         let min_x = self.min.x - 1;
+//         let max_x = self.max.x + 1;
+
+//         let min_y = self.min.y - 1;
+//         let max_y = self.max.y + 1;
+
+//         let min_z = self.max.z - 1;
+//         let max_z = self.max.z + 1;
+
+//         let iter = (min_x..=max_x)
+//             .into_iter()
+//             .map(move |x| (min_y..=max_y).into_iter().map(move |y| (x, y)))
+//             .flatten()
+//             .map(move |(x, y)| (min_z..=max_z).into_iter().map(move |z| (x, y, z)))
+//             .flatten()
+//             .map(|(x, y, z)| Point3::new(x, y, z));
+//         Box::new(iter)
+//     }
+// }
 
 #[derive(Debug)]
-struct Space {
-    world: HashSet<Point3>,
-    dimension: Option<Dimension>,
+struct Space<T>
+where
+    T: Point + DimensionBound,
+{
+    world: HashSet<T>,
+    dimension: Option<Dimension<T>>,
 }
 
-impl fmt::Display for Space {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        if let Some(d) = self.dimension.as_ref() {
-            writeln!(
-                f,
-                "Dimension: X: {}:{}, Y: {}:{}, Z: {}:{}",
-                d.min_x, d.max_x, d.min_y, d.max_y, d.min_z, d.max_z
-            );
-
-            for z in d.min_z + 1..d.max_z {
-                write!(f, "Layer {}\n", z);
-                for y in d.min_y + 1..d.max_y {
-                    for x in d.min_x + 1..d.max_x {
-                        if self.active(&Point3::new(x, y, z)) {
-                            write!(f, "#");
-                        } else {
-                            write!(f, ".");
-                        }
-                    }
-
-                    write!(f, "\n");
-                }
-            }
-
-            Ok(())
-        } else {
-            write!(f, "Empty fields")
-        }
-    }
-}
-
-impl Space {
+impl<T> Space<T>
+where
+    T: Point + DimensionBound + Clone + 'static,
+{
     fn new() -> Self {
         Self {
             world: HashSet::new(),
@@ -115,16 +193,17 @@ impl Space {
         for x in 0..input.width {
             for y in 0..input.height {
                 if let Some(Element::Active) = input.get(x, y) {
-                    let point = Point3::new(x as isize, y as isize, 0);
+                    let point = T::from_xy(x as isize, y as isize);
                     space.add_point(point);
                     // dbg!(&space.dimension);
                 }
             }
         }
+
         space
     }
 
-    fn add_point(&mut self, point: Point3) {
+    fn add_point(&mut self, point: T) {
         match self.dimension.as_mut() {
             Some(d) => d.extend(&point),
             None => self.dimension = Some(Dimension::from_point(&point)),
@@ -133,7 +212,7 @@ impl Space {
         self.world.insert(point);
     }
 
-    fn active(&self, p: &Point3) -> bool {
+    fn active(&self, p: &T) -> bool {
         self.world.contains(p)
     }
 
@@ -162,27 +241,41 @@ impl Space {
         space
     }
 
-    fn points(&self) -> Box<dyn Iterator<Item = Point3>> {
+    fn points(&self) -> Box<dyn Iterator<Item = T>> {
         if let Some(d) = &self.dimension {
-            let min_x = d.min_x;
-            let max_x = d.max_x;
-
-            let min_y = d.min_y;
-            let max_y = d.max_y;
-
-            let min_z = d.min_z;
-            let max_z = d.max_z;
-
-            let iter = (min_x..=max_x)
-                .into_iter()
-                .map(move |x| (min_y..=max_y).into_iter().map(move |y| (x, y)))
-                .flatten()
-                .map(move |(x, y)| (min_z..=max_z).into_iter().map(move |z| (x, y, z)))
-                .flatten()
-                .map(|(x, y, z)| Point3::new(x, y, z));
-            Box::new(iter)
+            d.points()
         } else {
-            Box::new(std::iter::empty::<Point3>())
+            Box::new(std::iter::empty::<T>())
+        }
+    }
+}
+
+impl fmt::Display for Space<Point3> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        if let Some(d) = self.dimension.as_ref() {
+            writeln!(
+                f,
+                "Dimension: X: {}:{}, Y: {}:{}, Z: {}:{}",
+                d.min.x, d.max.x, d.min.y, d.max.y, d.min.z, d.max.z
+            );
+
+            for z in d.min.z..=d.max.z {
+                write!(f, "Layer {}\n", z);
+                for y in d.min.y..=d.max.y {
+                    for x in d.min.x..=d.max.x {
+                        if self.active(&Point3::new(x, y, z)) {
+                            write!(f, "#");
+                        } else {
+                            write!(f, ".");
+                        }
+                    }
+
+                    write!(f, "\n");
+                }
+            }
+            Ok(())
+        } else {
+            write!(f, "Empty fields")
         }
     }
 }
@@ -204,13 +297,11 @@ fn parser(input: char) -> Option<Element> {
 fn main() -> Result<(), Box<dyn Error>> {
     let raw = fs::read_to_string("data/day17.txt")?;
     let grid = Grid::parse(&raw, parser).ok_or("can't parse input")?;
-    let mut space = Space::from_grid(grid);
+    let mut space: Space<Point3> = Space::from_grid(grid);
 
-    println!("before:\n{}", space);
-
-    for step in 0..6 {
+    for _ in 0..6 {
+        println!("{}", space);
         space = space.step();
-        // dbg!(&space);
     }
 
     dbg!(space.total());
@@ -224,9 +315,27 @@ mod test {
 
     #[test]
     fn test_iter_total() {
-        let point = Point::new(0, 0, 0);
+        let point = Point3::new(0, 0, 0);
         let total = point.around().count();
 
         assert_eq!(total, 26);
+    }
+
+    #[test]
+    fn test_dimension_points() {
+        let min = Point3::new(0, 0, 0);
+        let max = Point3::new(0, 0, 0);
+        let d = Dimension { min, max };
+
+        assert_eq!(d.points().count(), 27);
+    }
+
+    #[test]
+    fn test_dimension_points_more() {
+        let min = Point3::new(0, 0, 0);
+        let max = Point3::new(1, 2, 3);
+        let d = Dimension { min, max };
+
+        assert_eq!(d.points().count(), 120);
     }
 }
